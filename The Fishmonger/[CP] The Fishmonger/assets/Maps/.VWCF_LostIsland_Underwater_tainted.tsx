8c9476a24b3dd9d5000ea334ed0b0c191e4c731c
VoidWitchCult <127105133+VoidWitchCult@users.noreply.github.com>
2026-06-14 20:00:31 +0200

    0.5.2
    Ok, NOW this is 0.5.2 (hopefully).

---
+++ The Fishmonger/[CP] The Fishmonger/assets/Maps/.VWCF_LostIsland_Underwater_tainted.tsx
@@ -0,0 +1,243 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name=".VWCF_LostIsland_Underwater_tainted" tilewidth="16" tileheight="16" tilecount="832" columns="32">
+ <image source=".VWCF_LostIsland_Underwater_tainted.png" width="512" height="416"/>
+ <tile id="0">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="Type" value="Dirt"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="Type" value="Dirt"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <animation>
+   <frame tileid="19" duration="500"/>
+   <frame tileid="20" duration="500"/>
+   <frame tileid="21" duration="500"/>
+   <frame tileid="22" duration="500"/>
+   <frame tileid="23" duration="500"/>
+   <frame tileid="24" duration="500"/>
+   <frame tileid="25" duration="500"/>
+   <frame tileid="24" duration="500"/>
+   <frame tileid="23" duration="500"/>
+   <frame tileid="22" duration="500"/>
+   <frame tileid="21" duration="500"/>
+   <frame tileid="20" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="35">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="36">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="37">
+  <properties>
+   <property name="Type" value="Dirt"/>
+  </properties>
+ </tile>
+ <tile id="64">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="65">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="66">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="67">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="68">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="96">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="97">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="98">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="99">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="100">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="128">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="129">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="130">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="131">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="132">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="160">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="161">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="162">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="163">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="164">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="416">
+  <animation>
+   <frame tileid="416" duration="300"/>
+   <frame tileid="417" duration="300"/>
+   <frame tileid="418" duration="300"/>
+   <frame tileid="416" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="418">
+  <animation>
+   <frame tileid="418" duration="300"/>
+   <frame tileid="417" duration="300"/>
+   <frame tileid="416" duration="300"/>
+   <frame tileid="417" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="419">
+  <animation>
+   <frame tileid="419" duration="300"/>
+   <frame tileid="420" duration="300"/>
+   <frame tileid="421" duration="300"/>
+   <frame tileid="420" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="421">
+  <animation>
+   <frame tileid="421" duration="300"/>
+   <frame tileid="420" duration="300"/>
+   <frame tileid="419" duration="300"/>
+   <frame tileid="420" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="448">
+  <animation>
+   <frame tileid="448" duration="300"/>
+   <frame tileid="449" duration="300"/>
+   <frame tileid="450" duration="300"/>
+   <frame tileid="449" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="450">
+  <animation>
+   <frame tileid="450" duration="300"/>
+   <frame tileid="449" duration="300"/>
+   <frame tileid="448" duration="300"/>
+   <frame tileid="449" duration="300"/>
+  </animation>
+ </tile>
+</tileset>
